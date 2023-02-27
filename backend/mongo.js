d7718ee18f76d8c68860ef7929dbc4774674d468
CapucineFst <99402521+CapucineFst@users.noreply.github.com>
2023-02-27 14:42:49 +0100

    Added comments

--- backend/mongo.js
+++ backend/mongo.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
+//process.env for better security
 const USER = process.env.DB_USER;
 const PASSWORD = process.env.DB_PASSWORD;
 const DATABASE = process.env.DB_NAME;
 const uri = `mongodb+srv://${USER}:${PASSWORD}@${DATABASE}.lvrkxbu.mongodb.net/?retryWrites=true&w=majority`;
 
-//Connexion à mongooseDB
+/** Connexion to mongoose DB
+ * Send a message giving the status of the connexion
+*/
 mongoose.connect(uri)
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
