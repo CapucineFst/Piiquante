b2ee63c64d194effe573cd246fe28a22add98802
CapucineFst <99402521+CapucineFst@users.noreply.github.com>
2023-03-22 10:15:29 +0100

    Error management function

---
+++ backend/errors/error.js
@@ -0,0 +1,18 @@
+/**
+ * Catch the error message and return it in a clearer way
+ * @param {object} error 
+ * @returns object message
+ */
+const getErrorMessage = (error) => {
+    if (error.errors !== undefined ) {
+        const messages = error.errors.map(e => e.message);
+        return { message : messages.join(',')};
+    }
+    if(error.message !== undefined) 
+    {
+        return {message : error.message};
+    }
+    return {message : 'erreur inconnue'};
+};
+
+module.exports = getErrorMessage;
